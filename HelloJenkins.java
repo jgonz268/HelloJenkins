91c29a729f231f3d8f65471bb62c529e67a8a9d1
jgonz268 <jgonz268@outlook.com>
2017-06-20 01:47:17 +0000

    Adding HelloJenkins

---
+++ HelloJenkins.java
@@ -0,0 +1,11 @@
+
+public class HelloJenkins {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         for(int x =0; x<10; x++){
+        	 System.out.println("Hello Jenkins: "+ x);
+         }
+	}
+
+}
