1ad475815229aa65e17bb7f49bb1dc246e4c136a
jgonz268 <jgonz268@outlook.com>
2017-06-20 01:56:48 +0000

    New iteration

--- HelloJenkins.java
+++ HelloJenkins.java
@@ -3,7 +3,7 @@ public class HelloJenkins {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-         for(int x =0; x<10; x++){
+         for(int x =1; x<10; x++){
         	 System.out.println("Hello Jenkins: "+ x);
          }
 	}
